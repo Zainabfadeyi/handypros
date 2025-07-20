76c043746fba701e71bffbafaa17102f7220180f
Zainab Omolola Fadeyi <zainabfadeyi@gmail.com>
2025-07-20 23:36:23 +0100

    this is insane it worked

--- user/src/main/java/org/example/user/UserApplication.java
+++ user/src/main/java/org/example/user/UserApplication.java
@@ -3,8 +3,10 @@ package org.example.user;
 import lombok.experimental.SuperBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 
+@Configuration
 @SpringBootApplication
 public class UserApplication {
 
